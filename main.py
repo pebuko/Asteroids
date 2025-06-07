3e50ab1cf82a5a990d9e55e4b85372cbf027135c
pebuko <peterbusfy@seznam.cz>
2025-06-07 21:47:03 +0200

    bb1

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
-
+    print(f"Screen widht: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 if __name__ == "__main__":
     main()
