a04939c963de0a714c1ce1a1c1636da22ceb8683
pebuko <peterbusfy@seznam.cz>
2025-06-08 16:46:42 +0200

    added rotation

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
     
         screen.fill(0, rect=None, special_flags=0)
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
